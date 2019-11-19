5940ab2d4b8edadcefb34574c7ecf9adf80f88f0
Daniel <d.olugbade@outlook.com>
2019-11-19 23:09:29 +0000

    Added temporary css for home page

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './css/home.css'
 
 export default class Home extends React.Component {
 
-    
+
     render() {
         return (
-            <div>Home Page</div>
+            <div className="page-links">
+                <a href="http://www.google.com">Home Page</a>
+                <div className="horizontal-row">
+                <hr/>
+                </div>
+                <a href="http://www.google.com">Login Page</a>
+            </div>
         );
     }
 }
