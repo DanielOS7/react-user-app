68d96e7d1f666b8474be6d2b8641c13de6882553
Daniel <d.olugbade@outlook.com>
2019-11-20 15:42:18 +0000

    Basic login functionality added in frontend

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -1,15 +1,64 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import  { Redirect } from 'react-router-dom'
 
 
 
 export default class Login extends React.Component {
 
+    constructor(){
+        super();
 
-    onSubmit(e) {
+        this.state = {
+            username:"",
+            password: "",
+            name:"",
+            role: 0
+
+        };
+    }
+
+    onSubmit = (e) => {
         e.preventDefault();
-        console.log(e.target.formUsername.value);
         console.log('Was this called');
+
+        this.setState({
+            username: e.target.formUsername.value,
+            password: e.target.formPassword.value
+        }, () => {
+
+            let data = {
+                "username": this.state.username,
+                "password": this.state.password
+            }
+
+            console.log(JSON.stringify(data) + "attempted to be checked")
+    
+             fetch(`http://localhost:2700/login`,{
+                method:'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+    
+            })
+            .then(response => {
+ 
+
+                if (response.status === 200) {
+                   console.log('Success');
+                   window.location.replace(`http://localhost:3000`);
+                } else {
+                    alert('Login Failed');
+                };
+            } )
+        });
+
+         e.target.formUsername.value = "";
+         e.target.formPassword.value = "";
+
+
 
     }
 
@@ -19,10 +68,6 @@ export default class Login extends React.Component {
             .then(data => console.log(data))
     }
 
-    
-
-
-
 
     render() {
         return (
@@ -30,7 +75,7 @@ export default class Login extends React.Component {
                 <Form onSubmit={this.onSubmit}>
                     <Form.Group controlId="formUsername">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="input" placeholder="Enter Username" />
+                        <Form.Control type="input" placeholder="Username" />
                     </Form.Group>
                     <Form.Group controlId="formPassword">
                         <Form.Label>Password</Form.Label>
