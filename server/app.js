0de1f2fb0995d8d36d55e0f3cbd5952fad3e8452
Daniel <d.olugbade@outlook.com>
2019-11-20 13:57:39 +0000

    Added required endpoints for users table

--- server/app.js
+++ server/app.js
@@ -55,6 +55,61 @@ app.post("/addUser", function (request, response) {
   response.end();
 });
 
+app.put("/updateUser", function (request, response) {
+  console.log("Something");
+
+  console.log(request.body)
+  var username = request.body.username;
+  var password = request.body.password;
+  var name = request.body.name;
+  var role = request.body.role;
+
+  var sqlquery = `UPDATE users SET username = ${username},
+   password = ${password}, name = ${name}, role = ${role == 'admin' ? 1 : 2}  WHERE username =  ${username}  `;
+  console.log(sqlquery);
+  con.query(sqlquery, function (error, data) {
+    if (error) {
+      console.log('Error adding a new user');
+    }
+    else {
+      console.log('Updated user');
+    }
+  });
+  response.end();
+});
+
+
+app.post("/login", function (req, res) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(req.body)
+
+  var sqlquery = `select * from users where Username='${username}' AND Password='${password}'`;
+
+  console.log(sqlquery);
+  con.query(sqlquery, function (err, data) {
+
+    if (err) {
+      console.log(err);
+    }
+    else {
+      if (data.length == 0) {
+        console.log('Heyyyyy');;
+      }
+      else {
+        console.log('User Found')
+
+      }
+
+    }
+
+  });
+});
+
+
+
+
 
 
 app.listen(2700);
