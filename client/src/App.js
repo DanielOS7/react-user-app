ae762ad90ea4afd8e6aa01374b9847b7ccc7413d
Daniel <d.olugbade@outlook.com>
2019-11-21 10:16:39 +0000

    Created Navbar and made refactored it to be a stateless component

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,23 @@ import Login from './components/login.component';
 import CreateUser from './components/create-user.component';
 import Employee from './components/employee.component';
 import CreateEmployee from './components/create-employee.component';
-import NavBar from './components/navbar.component';
+import {NavBar} from './components/navbar.component';
 
 class App extends React.Component {
   render() {
-      return (
-        <div>
-          <NavBar/>
+    return (
+      <div>
+        <NavBar />
         <BrowserRouter>
-                  <Route exact path={"/"} component={Home} />
-                  <Route path={"/login"} component={Login} />
-                  <Route path={"/create-user"} component={CreateUser} />
-                  <Route path={"/employee"} component={Employee} />
-                  <Route path={"/create-employee"} component={CreateEmployee} />
-          </BrowserRouter>
+          <Route exact path={"/"} component={Home} />
+          <Route path={"/login"} component={Login} />
+          <Route path={"/create-user"} component={CreateUser} />
+          <Route path={"/employee"} component={Employee} />
+          <Route path={"/create-employee"} component={CreateEmployee} />
+        </BrowserRouter>
 
-        </div>
-      );
+      </div>
+    );
   }
 }
 
